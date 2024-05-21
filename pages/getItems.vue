a6d9f473f3cdac00992abcd64075462b7429a056
hagetouhi1108 <46263132+hagetouhi1108@users.noreply.github.com>
2024-05-21 11:06:26 +0900

    実データと同じ型で実装

---
+++ pages/getItems.vue
@@ -0,0 +1,12 @@
+<script setup>
+const { data: items } = useFetch('/api/fridge_items');
+</script>
+<template>
+  <div>
+    <h1>ここでは冷蔵庫アイテムたちの情報を取得して一覧表示できるよ！</h1>
+    <ul>
+      <li>id owner_name uploaded_at product_name eating_allowed image_url</li>
+      <li v-for="item in items" :key="item.id">{{ item.id + " " + item.owner_name + " " + item.uploaded_at + " " + item.product_name + " " + item.eating_allowed + " " + item.image_url }}</li>
+    </ul>
+  </div>
+</template>

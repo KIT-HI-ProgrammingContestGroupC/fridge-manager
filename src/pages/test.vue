26c29357ec15da6376e9193916a11cc279f6736a
ak1-foo <74660186+ak1-foo@users.noreply.github.com>
2024-06-07 11:39:34 +0900

    [WIP] feature-yahoo-api

---
+++ src/pages/test.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="error">
+    Error: {{ error.message }}
+  </div>
+  <div v-else-if="result">
+    {{ result.janCode }}
+  </div>
+  <div v-else>
+    Loading...
+  </div>
+</template>
+
+<script setup lang="ts">
+const { data: result, error, pending } = useFetch('/api/productInfoFromJanCode', {
+  query: {
+    janCode: 4901085614730,
+  },
+})
+// デバッグ用ログ
+console.log('result:', result)
+console.log('error:', error)
+console.log('pending:', pending)
+</script>
---
+++ src/server/api/productInfoFromJanCode.get.ts
@@ -0,0 +1,39 @@
+interface ProductInfo {
+  janCode: string
+  displayName: string
+  brand: string
+  company: string
+  imageUrl: string
+}
+
+export default defineEventHandler(async (event) => {
+  const query = getQuery(event)
+  try {
+    const response = await $fetch('https://shopping.yahooapis.jp/ShoppingWebService/V3/itemSearch', {
+      method: 'GET',
+      query: {
+        appid: useRuntimeConfig().yahooClientId,
+        jan_code: query.janCode,
+      },
+    })
+
+    console.log('my_response: ', response)
+
+    const productInfo: ProductInfo = {
+      janCode: query.janCode,
+      displayName: response.hits[0].name,
+      brand: response.hits[0].brand.name,
+      company: response.hits[0].parentBrands[1].name,
+      imageUrl: response.hits[0].images.medium,
+    }
+
+    return productInfo
+  }
+  catch (error) {
+    console.error('Error fetching product info:', error)
+    throw createError({
+      statusCode: 500,
+      statusMessage: 'An error occurred while fetching product info.',
+    })
+  }
+})

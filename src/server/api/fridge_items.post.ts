b104ed97e3aeae9eeb758a38c5d1a8ccf87b5799
hagetouhi1108 <46263132+hagetouhi1108@users.noreply.github.com>
2024-06-08 14:47:36 +0900

    lintによる整形

--- src/server/api/fridge_items.post.ts
+++ src/server/api/fridge_items.post.ts
@@ -4,16 +4,16 @@ export default defineEventHandler(async (event) => {
   const prisma = new PrismaClient()
 
   // 日付取得・整形
-  const now = new Date();
+  const now = new Date()
   const options: Intl.DateTimeFormatOptions = {
     year: 'numeric',
     month: 'long',
     day: 'numeric',
     hour: '2-digit',
     minute: '2-digit',
-    second: '2-digit'
-  };
-  const localeDateString = now.toLocaleDateString('ja-JP', options);
+    second: '2-digit',
+  }
+  const localeDateString = now.toLocaleDateString('ja-JP', options)
 
   const body = await readBody(event)
   const newItem = await prisma.fridge_items.create({

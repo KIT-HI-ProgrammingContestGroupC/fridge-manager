07f8ae9e1b52d1692cf0aa765ba80e703c8ff34e
hagetouhi1108 <46263132+hagetouhi1108@users.noreply.github.com>
2024-06-10 17:40:49 +0900

    migrationの不要なファイルをgit管理から除外

--- src/prisma/migrations/20240608054021_20240608_db/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "fridge_items" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "owner_name" TEXT NOT NULL,
-    "uploaded_at" TEXT NOT NULL,
-    "product_name" TEXT NOT NULL,
-    "eating_allowed" BOOLEAN NOT NULL DEFAULT false,
-    "image_url" TEXT NOT NULL
-);

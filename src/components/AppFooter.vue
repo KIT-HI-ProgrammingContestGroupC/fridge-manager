688f681401f092729fa20631b70045b45b1d7a63
ak1-foo <74660186+ak1-foo@users.noreply.github.com>
2024-06-25 18:46:20 +0900

    fix layout of footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,20 +1,25 @@
 <template>
-  <v-container
-    class="footer-container"
-  >
-    <v-footer>
-      <v-row
-        justify="center"
-      >
-        <v-btn
-          href="https://github.com/KIT-HI-ProgrammingContestGroupC/fridge-manager/issues"
-        >
-          Report Issue
-        </v-btn>
+  <v-footer>
+    <v-container
+      class="footer-container"
+    >
+      <v-row justify="center">
+        <v-col cols="auto">
+          <v-btn
+            href="https://github.com/KIT-HI-ProgrammingContestGroupC/fridge-manager/issues"
+          >
+            Report Issue
+          </v-btn>
+        </v-col>
       </v-row>
-      fridge-manager {{ appVersion }}
-    </v-footer>
-  </v-container>
+
+      <v-row justify="end">
+        <v-col cols="auto">
+          fridge-manager {{ appVersion }}
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-footer>
 </template>
 
 <script setup lang="ts">

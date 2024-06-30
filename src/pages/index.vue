732ba37ce4fdbe29d489459a961d732a31dc3439
ak1-foo <74660186+ak1-foo@users.noreply.github.com>
2024-06-30 19:26:49 +0900

    make form fields as an object

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -34,20 +34,19 @@
             <!-- Input area -->
             <v-card-text>
               <v-select
-                v-model="selectedMember"
+                v-model="formFields.owner_name.value"
                 label="Owner"
                 :items="members"
                 item-title="profile.real_name"
-                return-object
               />
               <v-text-field
-                v-model="product_name"
+                v-model="formFields.product_name.value"
                 hint="Enter your product name you want to put on the fridge"
                 label="Product Name"
                 type="input"
               />
               <v-switch
-                v-model="eating_allowed"
+                v-model="formFields.eating_allowed.value"
                 color="primary"
                 label="Take free?"
                 inset
@@ -59,7 +58,7 @@
                 @change="onFileUploaded"
               />
               <v-img
-                :src="image_url"
+                :src="formFields.image_url.value"
               />
             </v-card-text>
 
@@ -147,15 +146,14 @@ const filteredRows = computed(() => {
 // }
 
 const handleBarcodeDetected = async (data): Promise<void> => {
-  product_name.value = data.displayName
-  image_url.value = data.imageUrl
+  formFields.product_name.value = data.displayName
+  formFields.image_url.value = data.imageUrl
   showBarcodeReader.value = false
 }
 
-// members is an array of user objects, selectedMember is a user object
+// members is an array of user objects
 // about user object, see https://api.slack.com/methods/users.info#examples
 const { data: members } = useFetch('/api/slackMembers')
-const selectedMember = ref()
 
 // ==============================================
 // =============ここからDB関連のスクリプト===========
@@ -169,9 +167,19 @@ const fetchItems = async (): Promise<void> => {
 }
 
 // データ保存用の変数。データ追加フォームの各textbox等と紐づけられる。
-const product_name: Ref<string> = ref('')
-const eating_allowed: Ref<boolean> = ref(false)
-const image_url: Ref<string> = ref('')
+interface FormFields {
+  owner_name: Ref<string>
+  product_name: Ref<string>
+  eating_allowed: Ref<boolean>
+  image_url: Ref<string>
+}
+const formFields: FormFields = {
+  owner_name: ref(''),
+  product_name: ref(''),
+  eating_allowed: ref(false),
+  image_url: ref(''),
+}
+
 // エラーメッセージ保存用の変数。フォーム入力の不足・不正があればここに警告文を入れる。
 const errorMessage: Ref<string> = ref('')
 const image_binary: Ref<Blob | undefined> = ref(undefined)
@@ -181,7 +189,7 @@ const onFileUploaded = async (): Promise<void> => {
   const reader = new FileReader()
   reader.readAsDataURL(image_binary.value)
   reader.onload = (e) => {
-    image_url.value = e.target.result
+    formFields.image_url.value = e.target.result
   }
 }
 
@@ -189,10 +197,10 @@ const onFileUploaded = async (): Promise<void> => {
 // +ボタンのポップアップから各種値を入力した後、REGISTERボタンを押すと呼び出される
 // フォームに必要なデータが入力されているか確認した後、入力されていればDBに追加する
 const addItem = async (): Promise<void> => {
-  const oname = selectedMember.value.real_name // 所有者名
-  const pname = product_name.value // 製品名
-  const eallowed = eating_allowed.value // 共有許可
-  const iurl = image_url.value // 写真
+  const oname = formFields.owner_name.value // 所有者名
+  const pname = formFields.product_name.value // 製品名
+  const eallowed = formFields.eating_allowed.value // 共有許可
+  const iurl = formFields.image_url.value // 写真
 
   // ===フォーム確認======================================
   // 所有者名、製品名の2項目のみ確認しています。
@@ -227,10 +235,11 @@ const addItem = async (): Promise<void> => {
     })
 
     // 対応するテキストボックス等を空にする
-    selectedMember.value = ''
-    product_name.value = ''
-    eating_allowed.value = false
-    image_url.value = ''
+    formFields.owner_name.value = ''
+    formFields.product_name.value = ''
+    formFields.eating_allowed.value = false
+    formFields.image_url.value = ''
+    image_binary.value = undefined
 
     showPopup.value = false // ポップアップを閉じる
     fetchItems() // リストを更新

0b2ad3c028f83000ac14354829727f6dbb5a3079
ak1-foo <74660186+ak1-foo@users.noreply.github.com>
2024-07-03 18:48:45 +0900

    move product info type to type declare file

--- src/server/api/yahooAPIWrapper.get.ts
+++ src/server/api/yahooAPIWrapper.get.ts
@@ -1,13 +1,5 @@
 import type { YahooAPIResponse } from '~/types/yahooAPIResponse'
 
-interface ProductInfo {
-  janCode: string
-  displayName: string | undefined
-  brand: string | undefined
-  company: string | undefined
-  imageUrl: string | undefined
-}
-
 export default defineEventHandler(async (event) => {
   const query = getQuery(event)
   try {

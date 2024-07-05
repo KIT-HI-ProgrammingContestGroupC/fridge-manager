c558d800393852ce12c028d74e6d620283f499de
ak1-foo <74660186+ak1-foo@users.noreply.github.com>
2024-07-05 15:00:00 +0900

    add simple login page

---
+++ src/pages/login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h1>Login Page</h1>
+    <form @submit.prevent="signIn({ username, password })">
+      <div>
+        <label for="username">UserName:</label>
+        <input
+          id="username"
+          v-model="username"
+          type="text"
+          placeholder="Username"
+        >
+      </div>
+      <div>
+        <label for="password">Password:</label>
+        <input
+          id="password"
+          v-model="password"
+          type="password"
+          placeholder="Password"
+        >
+      </div>
+      <button type="submit">
+        sign in
+      </button>
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const { signIn } = useAuth()
+
+const username = ref('')
+const password = ref('')
+</script>

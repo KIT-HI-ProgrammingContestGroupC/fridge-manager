9bd55e56264dd929996b8b7505479731c940eb0b
ak1-foo <74660186+ak1-foo@users.noreply.github.com>
2024-07-05 16:47:01 +0900

    make login page more beautiful with vuetify

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,30 +1,39 @@
 <template>
-  <div>
-    <h1>Login Page</h1>
-    <form @submit.prevent="signIn({ username, password }, { callbackUrl: '/' })">
-      <div>
-        <label for="username">UserName:</label>
-        <input
-          id="username"
-          v-model="username"
-          type="text"
-          placeholder="Username"
-        >
-      </div>
-      <div>
-        <label for="password">Password:</label>
-        <input
-          id="password"
-          v-model="password"
-          type="password"
-          placeholder="Password"
-        >
-      </div>
-      <button type="submit">
-        sign in
-      </button>
-    </form>
-  </div>
+  <v-container>
+    <v-form
+      @submit.prevent="signIn({ username, password }, { callbackUrl: '/' })"
+    >
+      <v-card
+        class="mx-auto"
+        width="400"
+      >
+        <v-card-text>
+          <v-text-field
+            v-model="username"
+            label="Username"
+            prepend-icon="mdi-fridge"
+            type="text"
+          />
+          <v-text-field
+            v-model="password"
+            label="Password"
+            prepend-icon="mdi-lock"
+            type="password"
+          />
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer />
+          <v-btn
+            text="Sign In"
+            color="primary"
+            variant="flat"
+            type="submit"
+          />
+        </v-card-actions>
+      </v-card>
+    </v-form>
+  </v-container>
 </template>
 
 <script setup>
